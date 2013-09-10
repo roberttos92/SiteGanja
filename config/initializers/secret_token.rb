74a0c03155a15f7a27bb0b7468182f0919cc8e45
Bruno <bruno@zord>
2013-09-09 21:55:51 -0300

    Rails 4 puro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SiteGanja::Application.config.secret_key_base = 'f414e929638a1de93ce05c7be79fc7834571aa813fcd2bbb736bbe947867ac92d588de2c855933573dd82cc70942f7d5457dafdbb443ca321b3d04226cb3a2bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SiteGanja::Application.config.session_store :cookie_store, key: '_SiteGanja_session'
